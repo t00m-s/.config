5d0a893269d3730c099fb2dd7e565113fc957582
Tommaso <soncintommaso@gmail.com>
2023-09-17 13:00:36 +0200

    updated cmp nvim, waybar polling network

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,40 +1,54 @@
-local lsp = require('lsp-zero').preset({
-    manage_nvim_cmp = {
-        set_sources = 'reccomended'
-    }
-})
+local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({buffer = bufnr})
-    local opts = {buffer = bufnr, remap = false}
-    vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
 lsp.setup()
--- You need to setup `cmp` after lsp-zero
-local cmp = require('cmp')
+
+--- LSP Management
+require("mason").setup()
+require("mason-lspconfig").setup({
+	handlers = {
+		lsp.default_setup,
+	},
+})
+--- You need to setup `cmp` after lsp-zero
+local cmp = require("cmp")
 cmp.setup({
-    sources = {
-         { name = 'nvim_lsp'},
-    },
-    mapping = {
-        -- Tab key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm( {select = true }),
-        -- Navigate between snippet placeholder
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    },
-    completion = {
-        completeopt = 'menu,menuone,noinsert',
-    },
-    formatting = {
-        fields = {'abbr', 'kind', 'menu'},
-        format = require('lspkind').cmp_format({
-            mode = 'symbol', -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
-        })
-    }
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+	},
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- Tab key to confirm completion
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		-- Navigate between snippet placeholder
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+		-- Scroll docs
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
+	formatting = {
+		fields = { "abbr", "kind", "menu" },
+		format = require("lspkind").cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+		}),
+	},
 })
--- nvim/lua/tom/lazy.lua
+++ nvim/lua/tom/lazy.lua
@@ -38,18 +38,22 @@ return require("lazy").setup({
 	"mbbill/undotree",
 	"tpope/vim-fugitive",
 	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		dependencies = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ "williamboman/mason.nvim" }, -- Optional
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
-
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
+		{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+		-- LSP Support
+		{
+			"neovim/nvim-lspconfig",
+			dependencies = {
+				{ "hrsh7th/cmp-nvim-lsp" },
+			},
+		},
+		-- Autocompletion
+		{
+			"hrsh7th/nvim-cmp",
+			dependencies = {
+				{ "L3MON4D3/LuaSnip" },
+			},
 		},
 	},
 	{
