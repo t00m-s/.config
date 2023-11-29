e49f2c96c45e2ab404c5f16049033a426a954aef
Tommaso <soncintommaso@gmail.com>
2023-11-29 12:51:43 +0100

    updated bindings, less transparent

--- nvim/lua/custom/bindings.lua
+++ nvim/lua/custom/bindings.lua
@@ -1,41 +1,52 @@
-vim.api.nvim_set_keymap('n', '<leader>fs', ':w<CR>', { noremap = true, silent = true }) -- space fs to save file
-vim.api.nvim_set_keymap('n', '<leader>qq', ':q<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>qa', ':qa!<CR>', { noremap = true })
+local nvim_set_keymap = vim.api.nvim_set_keymap
+local vim_keymap_set = vim.keymap.set
+local opts = { noremap = true, silent = true }
+nvim_set_keymap('n', '<leader>fs', ':w<CR>', opts) -- space fs to save file
+nvim_set_keymap('n', '<leader>qq', ':q<CR>', { noremap = true })
+nvim_set_keymap('n', '<leader>qa', ':qa!<CR>', { noremap = true })
 -- Copy and paste to system clipboard
-vim.api.nvim_set_keymap('n', '<leader>cc', '"+y', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>cv', '"+p', { noremap = true })
+nvim_set_keymap('n', '<leader>cc', '"+y', { noremap = true })
+nvim_set_keymap('n', '<leader>cv', '"+p', { noremap = true })
 
-vim.api.nvim_set_keymap('v', '<leader>cc', '"+y', { noremap = true })
-vim.api.nvim_set_keymap('v', '<leader>cv', '"+p', { noremap = true })
+nvim_set_keymap('v', '<leader>cc', '"+y', { noremap = true })
+nvim_set_keymap('v', '<leader>cv', '"+p', { noremap = true })
+
 -- When deleting with x, this does not save to registers
-vim.api.nvim_set_keymap('n', 'x', '"_x', { noremap = true })
+nvim_set_keymap('n', 'x', '"_x', { noremap = true })
 
 -- Opens the file manager in nvim
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim_keymap_set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Split windows management
-vim.keymap.set('n', '<leader>sv', vim.cmd.vsp)
-vim.keymap.set('n', '<leader>sh', vim.cmd.sp)
+vim_keymap_set('n', '<leader>sv', vim.cmd.vsp)
+vim_keymap_set('n', '<leader>sh', vim.cmd.sp)
 -- Resize windows size
-vim.api.nvim_set_keymap('n', '<Right>', ':vertical resize +5<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Left>', ':vertical resize -5<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Down>', ':horizontal resize -5<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Up>', ':horizontal resize +5<CR>', { noremap = true, silent = true })
+nvim_set_keymap('n', '<Right>', ':vertical resize +5<CR>', opts)
+nvim_set_keymap('n', '<Left>', ':vertical resize -5<CR>', opts)
+nvim_set_keymap('n', '<Down>', ':horizontal resize -5<CR>', opts)
+nvim_set_keymap('n', '<Up>', ':horizontal resize +5<CR>', opts)
 
 -- Switch to a different window
-vim.api.nvim_set_keymap('n', '<C-Left>', '<C-W>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Up>', '<C-W>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Down>', '<C-W>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Right>', '<C-W>l', { noremap = true, silent = true })
+nvim_set_keymap('n', '<C-Left>', '<C-W>h', opts)
+nvim_set_keymap('n', '<C-Up>', '<C-W>j', opts)
+nvim_set_keymap('n', '<C-Down>', '<C-W>k', opts)
+nvim_set_keymap('n', '<C-Right>', '<C-W>l', opts)
 
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+-- Space is now only used as a leader key
+vim_keymap_set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim_keymap_set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim_keymap_set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim_keymap_set('n', '[d', vim.diagnostic.goto_prev, { noremap = true, silent = true, desc = 'Go to previous diagnostic message' })
+vim_keymap_set('n', ']d', vim.diagnostic.goto_next, { noremap = true, silent = true, desc = 'Go to next diagnostic message' })
+vim_keymap_set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true, desc = 'Open floating diagnostic message' })
+
+-- Select all
+vim_keymap_set('n', '<C-a>', 'ggVG', { desc = 'Select all' })
+-- Tab switch buffer
+vim_keymap_set('n', 'te', ':tabedit<CR>', { desc = '[T]ab  [E]dit' })
+vim_keymap_set('n', '<TAB>', ':bnext<CR>', { desc = 'Next buffer' })
+vim_keymap_set('n', '<S-TAB>', ':bprevious<CR>', { desc = 'Previous buffer' })
