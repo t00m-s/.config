db32d618172594e8c5ce2daaf3cdd9e9e91b67ed
Tommaso <soncintommaso@gmail.com>
2024-02-15 18:34:30 +0100

    added nvim-textojects

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -18,5 +18,17 @@ vim.defer_fn(function()
       end,
     },
     indent = { enable = true, disable = { 'dart' } },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+    },
   }
 end, 0)
