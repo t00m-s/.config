afecc59500658282615b92c3cbd94d3d13a8facc
Tommaso <soncintommaso@gmail.com>
2024-02-27 10:29:58 +0100

    disabled copilot

--- .config/nvim/lua/custom/plugins/copilot.lua
+++ .config/nvim/lua/custom/plugins/copilot.lua
@@ -7,12 +7,13 @@ return {
         panel = { enabled = false },
       }
     end,
+    enabled = false,
   },
-
   {
     'zbirenbaum/copilot-cmp',
     config = function()
       require('copilot_cmp').setup()
     end,
+    enabled = false,
   },
 }
