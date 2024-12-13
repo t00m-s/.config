c524af77142df1a3e928dad2b1896d141a286ede
Tommaso <soncintommaso@gmail.com>
2024-12-13 21:24:30 +0100

    update conf

---
+++ nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+  },
+}
