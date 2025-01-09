8d8925015e74272b3fa5f5aec44333b6655e9e8d
Tommaso <soncintommaso@gmail.com>
2025-01-09 16:08:52 +0100

    finally fzf works to my liking

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -137,7 +137,16 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- ruff = {},
-        -- clangd = {},
+        clangd = {
+          cmd = {
+            'clangd',
+            '--background-index',
+            '--clang-tidy',
+            '--completion-style=detailed',
+            '--header-insertion=iwyu',
+            '--suggest-missing-includes',
+          },
+        },
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
--- nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/lua/kickstart/plugins/telescope.lua
@@ -5,12 +5,11 @@ return {
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+      {
+        -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
+        cond = function() end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
@@ -61,7 +60,25 @@ return {
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        local function find_project_root(start_dir)
+          local dir = start_dir
+          while dir do
+            if vim.fn.isdirectory(dir .. '/.git') == 1 then
+              return dir
+            end
+            local parent = vim.fn.fnamemodify(dir, ':h')
+            if parent == dir then
+              break -- Reached the filesystem root
+            end
+            dir = parent
+          end
+          return nil
+        end
+
+        local cwd = find_project_root(vim.loop.cwd()) or vim.loop.cwd()
+        builtin.find_files { cwd = cwd }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
