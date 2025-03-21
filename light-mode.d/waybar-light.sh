a80cda70d3c86f5d7084f9aad6be64ed25e6d621
Tommaso Soncin <soncintommaso@gmail.com>
2025-03-21 10:48:02 +0100

    add pipefail to scripts

--- light-mode.d/waybar-light.sh
+++ light-mode.d/waybar-light.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 sed -i 's|@import "mocha.css";|@import "latte.css";|' ~/.config/waybar/style.css
-
 if pgrep -x "waybar" > /dev/null; then
     killall waybar
 fi
