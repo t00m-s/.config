b7389f5ecf43c5888089df77b160687c14c3d0c0
Tommaso Soncin <soncintommaso@gmail.com>
2025-03-28 16:39:05 +0100

    add gtk toggl

--- light-mode.d/gtk-light.sh
+++ light-mode.d/gtk-light.sh
@@ -2,3 +2,4 @@
 set -euxo pipefail
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 gsettings set org.gnome.desktop.interface gtk-theme 'catppuccin-latte-peach-standard+default'
+sed -i "s|gtk-theme-name=catppuccin-mocha-peach-standard+default|gtk-theme-name=catppuccin-latte-peach-standard+default|" ~/.config/gtk-3.0/settings.ini
