3ac511ba1dd7bc7c85692b720e51febce057b6a3
Tommaso Soncin <soncintommaso@gmail.com>
2025-04-10 09:29:28 +0200

    chore: using include in mako config

--- dark-mode.d/mako-dark.sh
+++ dark-mode.d/mako-dark.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-CONFIG="$HOME/.config/mako/config"
-
-sed -i \
-    -e 's/background-color=.*/background-color=#1e1e2e/' \
-    -e 's/text-color=.*/text-color=#cdd6f4/' \
-    -e 's/border-color=.*/border-color=#fab387/' \
-    -e 's/progress-color=.*/progress-color=over #313244/' \
-    -e '/\[urgency=high\]/!b;n;s/border-color=.*/border-color=#fab387/' \
-    "$CONFIG"
-
+sed -i 's|include=/home/tommaso/.config/mako/themes/catppuccin-latte|include=/home/tommaso/.config/mako/themes/catppuccin-mocha|' ~/.config/mako/config
 # Reload mako
 pkill -SIGUSR1 mako
--- light-mode.d/mako-light.sh
+++ light-mode.d/mako-light.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-CONFIG="$HOME/.config/mako/config"
-
-sed -i \
-    -e 's/background-color=.*/background-color=#eff1f5/' \
-    -e 's/text-color=.*/text-color=#4c4f69/' \
-    -e 's/border-color=.*/border-color=#fe640b/' \
-    -e 's/progress-color=.*/progress-color=over #ccd0da/' \
-    -e '/\[urgency=high\]/!b;n;s/border-color=.*/border-color=#fe640b/' \
-    "$CONFIG"
-
+sed -i 's|include=/home/tommaso/.config/mako/themes/catppuccin-mocha|include=/home/tommaso/.config/mako/themes/catppuccin-latte|' ~/.config/mako/config
 # Reload mako
 pkill -SIGUSR1 mako
