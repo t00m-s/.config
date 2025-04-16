7ba9dfe34e044117213457b91862e1a3b7e0ba03
Tommaso Soncin <soncintommaso@gmail.com>
2025-04-16 15:00:01 +0200

    nuked uwsm

--- dark-mode.d/waybar-dark.sh
+++ dark-mode.d/waybar-dark.sh
@@ -2,10 +2,5 @@
 set -euxo pipefail
 sed -i 's|@import "latte.css";|@import "mocha.css";|' ~/.config/waybar/style.css
 if pgrep -x "waybar" >/dev/null; then
-    pkill -USR1 waybar || {
-        # Fallback to restart if reload isn't supported
-        killall waybar
-        sleep 0.5
-        uwsm app -- waybar &
-    }
+    pkill -USR1 waybar
 fi
