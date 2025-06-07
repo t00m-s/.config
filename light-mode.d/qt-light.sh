c854aa006dcefb5781cc17fe1dd0541e98b95764
Tommaso Soncin <soncintommaso@gmail.com>
2025-06-07 12:17:37 +0200

    fix: qt script

--- light-mode.d/qt-light.sh
+++ light-mode.d/qt-light.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 # qt5
 sed -i 's|color_scheme_path=/home/tommaso/.config/qt5ct/colors/catppuccin-mocha.conf|color_scheme_path=/home/tommaso/.config/qt5ct/colors/catppuccin-latte.conf|' ~/.config/qt5ct/qt5ct.conf
-sed -i 's|icon_theme=breeze-dark|icon_theme=breeze' ~/.config/qt5ct/qt5ct.conf
+sed -i 's|icon_theme=breeze-dark|icon_theme=breeze|' ~/.config/qt5ct/qt5ct.conf
 # qt6
 sed -i 's|color_scheme_path=/home/tommaso/.config/qt6ct/colors/catppuccin-mocha.conf|color_scheme_path=/home/tommaso/.config/qt6ct/colors/catppuccin-latte.conf|' ~/.config/qt6ct/qt6ct.conf
-sed -i 's|icon_theme=breeze-dark|icon_theme=breeze' ~/.config/qt6ct/qt6ct.conf
+sed -i 's|icon_theme=breeze-dark|icon_theme=breeze|' ~/.config/qt6ct/qt6ct.conf
