1e045dce0e329419df932a7d77262a619b7c1436
Tommaso Soncin <soncintommaso@gmail.com>
2025-06-09 18:16:41 +0200

    update(scripts): updated light and dark switch

--- light-mode.d/qt-light.sh
+++ light-mode.d/qt-light.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 # qt5
-sed -i 's|color_scheme_path=/home/tommaso/.config/qt5ct/colors/catppuccin-mocha-peach.conf|color_scheme_path=/home/tommaso/.config/qt5ct/colors/catppuccin-latte-peach.conf|' ~/.config/qt5ct/qt5ct.conf
-sed -i 's|icon_theme=breeze-dark|icon_theme=breeze' ~/.config/qt5ct/qt5ct.conf
+sed -i 's|color_scheme_path=/home/tommaso/.config/qt5ct/colors/catppuccin-mocha.conf|color_scheme_path=/home/tommaso/.config/qt5ct/colors/catppuccin-latte.conf|' ~/.config/qt5ct/qt5ct.conf
+sed -i 's|icon_theme=breeze-dark|icon_theme=breeze|' ~/.config/qt5ct/qt5ct.conf
 # qt6
-sed -i 's|color_scheme_path=/home/tommaso/.config/qt6ct/colors/catppuccin-mocha-peach.conf|color_scheme_path=/home/tommaso/.config/qt6ct/colors/catppuccin-latte-peach.conf|' ~/.config/qt6ct/qt6ct.conf
-sed -i 's|icon_theme=breeze-dark|icon_theme=breeze' ~/.config/qt6ct/qt6ct.conf
+sed -i 's|color_scheme_path=/home/tommaso/.config/qt6ct/colors/catppuccin-mocha.conf|color_scheme_path=/home/tommaso/.config/qt6ct/colors/catppuccin-latte.conf|' ~/.config/qt6ct/qt6ct.conf
+sed -i 's|icon_theme=breeze-dark|icon_theme=breeze|' ~/.config/qt6ct/qt6ct.conf
---
+++ light-mode.d/wlogout-light.sh
@@ -0,0 +1,2 @@
+set -euxo pipefail
+sed -i 's|@import "mocha.css";|@import "latte.css";|' ~/.config/wlogout/style.css
